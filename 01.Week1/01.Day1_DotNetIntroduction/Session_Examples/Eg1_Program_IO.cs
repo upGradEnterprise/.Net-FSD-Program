cf005feebb10a6442f4bc514b39e17475cb98841
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-07-09 13:08:22 +0530

    Add files via upload

---
+++ 01.Week1/01.Day1_DotNetIntroduction/Session_Examples/Eg1_Program_IO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+
+            Console.WriteLine("Enter your name : ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age : ");
+            age = int.Parse(Console.ReadLine());
+
+
+            //    Console.WriteLine("Hi " + name + ", Good afternoon...!");
+            Console.WriteLine($"Hi {name} , Good afternoon...!");
+
+            Console.WriteLine($"User Details :  {name}, {age}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
