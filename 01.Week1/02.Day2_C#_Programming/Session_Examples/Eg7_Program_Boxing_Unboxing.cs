a160afa033234c69c846be5c0c47eb1b41468711
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-07-10 17:33:37 +0530

    Add files via upload

---
+++ 01.Week1/02.Day2_C#_Programming/Session_Examples/Eg7_Program_Boxing_Unboxing.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+     class Program
+    {
+        static void Main()
+        {
+            int x = 10;
+            bool y = false;
+            double z = 5454.45;
+
+            object obj;
+
+
+            obj = x;    // boxing 
+            int a = (int) obj;  // unboxing
+            //  bool a = (bool)obj;     // no error,  throws exception : Invalid Cast Excpetion
+            Console.WriteLine($"A = {a}");
+
+
+            obj = y; // boxing 
+            bool b = (bool) obj; // unboxing
+            Console.WriteLine($"B = {b}");
+
+
+            obj = z; // boxing 
+            double c = (double)obj; // unboxing
+            Console.WriteLine($"C = {c}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 01.Week1/02.Day2_C#_Programming/Session_Examples/Eg8_Program_Boxing_Unboxing_IsOperator_ArrayList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+     class Program
+    {
+        static void Main()
+        {
+            ArrayList arrList = new ArrayList();
+            arrList.Add(1);
+            arrList.Add(2);
+            arrList.Add("three");
+            arrList.Add(4);
+            arrList.Add(false);  
+            
+            int sum = 0;
+
+            foreach (object item in arrList)
+            {
+                if(item is int n)
+                {
+                    sum += n;
+                    Console.WriteLine($"Processed int: {n}");
+                }
+                else
+                {
+                    Console.WriteLine($"Non-int value: {item}");
+                }
+               
+            }
+
+            Console.WriteLine($"Sum Result : {sum}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
