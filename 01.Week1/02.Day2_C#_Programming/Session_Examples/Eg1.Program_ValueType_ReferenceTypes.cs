b735b2226d1f5801e85f5eba0167ed108e89e351
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-07-10 10:19:38 +0530

    Add files via upload

---
+++ 01.Week1/02.Day2_C#_Programming/Session_Examples/Eg1.Program_ValueType_ReferenceTypes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 5;
+            int b = a;
+
+            Console.WriteLine($"a = {a}, b = {b}");
+            b = 15;
+
+            Console.WriteLine($"a = {a}, b = {b}");
+            Console.WriteLine("------------------------");
+
+
+            string s1 = "Hello";
+            string s2 = s1;
+
+            Console.WriteLine($"s1 = {s1}, s2 = {s2}");
+            s2 = "World";
+            Console.WriteLine($"s1 = {s1}, s2 = {s2}"); 
+
+         
+
+            Console.WriteLine("------------------------");
+            object obj1 = 123;
+
+            object obj2 = obj1;
+            Console.WriteLine($"obj1 = {obj1}, obj2 = {obj2}");
+
+            obj2 = 456;
+            Console.WriteLine($"obj1 = {obj1}, obj2 = {obj2}");
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 01.Week1/02.Day2_C#_Programming/Session_Examples/Eg2_Program_ReferenceTypes_Assignments.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    class  Student
+    {
+        public int Id;
+        public string Name;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.Name = "Scott";
+            s1.Id = 10256;
+
+
+            Student s2 = s1;
+            Console.WriteLine($"s1 - Name :  {s1.Name}, s2 - Name :  {s2.Name}, ");
+
+            s2.Name = "Smith";
+            Console.WriteLine($"s1 - Name :  {s1.Name}, s2 - Name :  {s2.Name}, ");
+            
+            Console.ReadLine();
+
+        }
+    }
+}
