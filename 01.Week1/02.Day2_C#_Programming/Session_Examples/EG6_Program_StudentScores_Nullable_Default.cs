0a0683accdb5cce2a6a9e9e59e8648772bb3248a
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-07-10 12:33:03 +0530

    Add files via upload

---
+++ 01.Week1/02.Day2_C#_Programming/Session_Examples/EG6_Program_StudentScores_Nullable_Default.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+     class Program
+    {
+
+        static void DisplayResult(string studentName, int? score = default)
+        {
+            string name = studentName;
+
+            //  int displayScore = score.HasValue ? score.Value : 0;
+            int displayScore = score ?? 0;    
+
+            Console.WriteLine($"Student: {name}");
+
+            if (score.HasValue)
+            {
+                Console.WriteLine($"Score: {displayScore}/100");
+            }
+            else
+            {
+                Console.WriteLine("No score available yet.");
+            }
+
+            Console.WriteLine("------------------------------");
+
+        }
+        
+
+        static void Main()
+        {
+
+            DisplayResult("Scott", 86);
+            DisplayResult("Smith");
+            DisplayResult("Sam",  69);
+            DisplayResult("Bob");
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 01.Week1/02.Day2_C#_Programming/Session_Examples/Eg5_Program_Nullable_Default.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+     class Program
+    { 
+        static void Main()
+        {
+
+            int? x = default;
+
+            if (x.HasValue)
+            {
+                Console.WriteLine($"X value : {x}");
+            }
+
+
+            int y = default;
+            Console.WriteLine($"Y value : {y}");
+
+            Console.ReadLine();
+        }
+    }
+}
