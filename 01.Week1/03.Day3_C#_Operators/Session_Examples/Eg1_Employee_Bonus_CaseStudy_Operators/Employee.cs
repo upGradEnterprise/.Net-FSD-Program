945a242c046dc35ed20cbb77abf9bd4068f5ea63
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-07-11 12:40:20 +0530

    Add files via upload

---
+++ 01.Week1/03.Day3_C#_Operators/Session_Examples/Eg1_Employee_Bonus_CaseStudy_Operators/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    internal class Employee
+    {
+        public decimal Salary;
+        public int NoOfYearsExp;
+        public bool IsHighPerformer;
+
+        public bool ValidateEmployeeData()
+        {
+            bool isValid = false;
+
+            if( (NoOfYearsExp >= 1 && NoOfYearsExp <= 40) && Salary >= 10000)
+            {
+                isValid = true; 
+               
+            }
+            
+            return isValid;
+        }
+
+        public decimal GetBonus()
+        {
+            decimal bonus = 0;
+
+            if (IsHighPerformer || (NoOfYearsExp >= 5  &&  Salary > 30000) ) {
+                bonus = Salary * 0.1m;
+            }
+                       
+            return bonus;
+        }
+    
+    
+    
+        public void DisplayDetails(decimal bonusAmount)
+        {
+            Console.WriteLine($"Actual Salary:  {Salary:F2}");
+            Console.WriteLine($"Years of Exp. :  {NoOfYearsExp}  years");
+            Console.WriteLine($"Is High Performer :  {IsHighPerformer}");
+            Console.WriteLine($"Bonus Amount :  {bonusAmount:F2}");
+        }
+    }
+}
---
+++ 01.Week1/03.Day3_C#_Operators/Session_Examples/Eg1_Employee_Bonus_CaseStudy_Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp37
+{
+     class Program
+    {
+        static void Main()
+        {
+            Employee empObj = new Employee();
+            empObj.NoOfYearsExp = 6;
+            empObj.Salary = 35000;
+            empObj.IsHighPerformer = false;
+
+
+            if( empObj.ValidateEmployeeData() == false)
+            {
+                Console.WriteLine("Invalid Employee Data");
+                Console.ReadLine();
+                return;
+            }
+        
+
+            decimal bonusAmount = empObj.GetBonus();
+
+            empObj.DisplayDetails(bonusAmount);
+
+
+            Console.ReadLine();
+        }
+    }
+
+}
