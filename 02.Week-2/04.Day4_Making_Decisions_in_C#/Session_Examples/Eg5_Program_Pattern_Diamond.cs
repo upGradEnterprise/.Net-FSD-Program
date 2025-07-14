f5cda11c3f9592226d5e4e1cae372e276d66a688
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-07-14 17:09:51 +0530

    Add files via upload

---
+++ 02.Week-2/04.Day4_Making_Decisions_in_C#/Session_Examples/Eg5_Program_Pattern_Diamond.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace ConsoleApp39
+{ 
+    class Program
+    {
+        static void Main()
+        {
+              Console.Write("Enter the number of rows for the diamond (half): ");
+              int n = Convert.ToInt32(Console.ReadLine());
+
+           
+
+            // Upper Part of the diamond
+            for (int i = 1; i <= n; i++)
+            {
+                for(int j = 1; j <= n - i; j++)
+                {
+                    Console.Write("  ");
+                }
+
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+
+            // Bottom Part of the diamond
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= n - i; j++)
+                {
+                    Console.Write("  ");
+                }
+
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 02.Week-2/04.Day4_Making_Decisions_in_C#/Session_Examples/Eg6_Program_Pattern_Diamond_2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp38
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+             Console.Write("Enter the number of rows for the diamond (half): ");
+             int n = Convert.ToInt32(Console.ReadLine());
+
+         
+
+            // Upper Part of the diamond
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n - i; j++)
+                {
+                    Console.Write("* ");
+                }
+
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("  ");
+                }
+
+                for (int l = 1; l <= n - i; l++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+
+            // Bottom Part of the diamond
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= n - i; j++)
+                {
+                    Console.Write("* ");
+                }
+
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("  ");
+                }
+
+                for (int l = 1; l <= n - i; l++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
