6620c14cabbe794715e71dbcfe3e3d4369e9d137
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-07-15 10:54:41 +0530

    Add files via upload

---
+++ 02.Week-2/05.Day5_Methods_in_C#/Session_Examples/Eg1_Program_Methods_BasicDemo.cs
@@ -0,0 +1,51 @@
+﻿
+
+
+namespace ConsoleApp39
+{
+    internal class Program
+    {
+        static void Greeting()
+        {
+            Console.WriteLine("Welcome to C# Methods");
+        }
+
+        static void GreetingByName(string name)
+        {
+            Console.WriteLine($"Hi {name}, Good morning...!");
+        }
+
+        static int GetSum(int x, int y)
+        {
+            int z = x + y;
+            return z;
+        }
+
+        static string GetCurrentTime()
+        {
+            string str = DateTime.Now.ToString("T");
+            return str;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Greeting();
+            Greeting();
+            Console.WriteLine("-------------------------");
+            GreetingByName("Ralph");
+            GreetingByName("Scott");
+            Console.WriteLine("-------------------------");
+
+            int result = GetSum(10, 20);
+            Console.WriteLine($"Sum Result : {result}");
+
+            Console.WriteLine("-------------------------");
+
+            string strCurrentTime = GetCurrentTime();
+            Console.WriteLine($"Current Time : " + strCurrentTime);
+
+            Console.ReadLine();
+        }
+    }
+}
