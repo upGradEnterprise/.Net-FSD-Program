deafe954305438085ab0465df92bcb6a524df3b9
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-07-17 10:57:10 +0530

    Add files via upload

---
+++ 02.Week-2/07.Day7_OOPS_Inheritance_Abstract/Session_Examples/Eg1_Program_Inheritance_BasicDemo.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp39
+{
+
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Employee : Person
+    {
+        public int EmployeeId { get; set; }
+        public string Job { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Employee employee = new Employee();
+            employee.EmployeeId = 6445454;
+            employee.Name = "Narasimha Rao";
+            employee.Age = 32;
+            employee.Job = "Manager";
+
+
+            Console.WriteLine($" {employee.EmployeeId},  {employee.Name}, {employee.Job}, {employee.Age}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 02.Week-2/07.Day7_OOPS_Inheritance_Abstract/Session_Examples/Eg2_Program_Usage_Of_Base_Keyword.cs
@@ -0,0 +1,53 @@
+﻿namespace ConsoleApp39
+{
+
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+     //   public Person( )  => Name = string.Empty;
+        public Person(string name)  => Name = name;
+
+        public virtual void ShowDetails() => Console.WriteLine($"Name : {Name}, Age : {Age}");
+
+    }
+
+
+
+    class Employee : Person
+    {
+        public int EmployeeId { get; set; }
+        public string Job { get; set; }
+
+        public Employee(string name) : base(name)
+        {
+
+        }
+
+        public override void ShowDetails()
+        {
+            base.ShowDetails();
+            Console.WriteLine($"Employee Id : {EmployeeId}, Job : {Job}");
+        }
+
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Employee employee = new Employee("Narasimha Rao");
+            employee.EmployeeId = 6445454; 
+            employee.Age = 32;
+            employee.Job = "Manager";
+
+
+            employee.ShowDetails();
+
+            Console.ReadLine();
+        }
+    }
+}
