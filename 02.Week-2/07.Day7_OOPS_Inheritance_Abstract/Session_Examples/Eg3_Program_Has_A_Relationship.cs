b785a30e357319b1491deb311d598c857ce92491
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-07-17 12:57:57 +0530

    Add files via upload

---
+++ 02.Week-2/07.Day7_OOPS_Inheritance_Abstract/Session_Examples/Eg3_Program_Has_A_Relationship.cs
@@ -0,0 +1,72 @@
+﻿using System.Net;
+
+namespace ConsoleApp39
+{
+    class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string PinCode { get; set; }
+
+        public Address(string street, string city, string pinCode)
+        {
+            Street = street;
+            City = city;
+            PinCode = pinCode;
+        }
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine($"Address: {Street}, {City}, {PinCode}");
+        }
+    }
+
+    class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public Address EmployeeAddress { get; set;  }
+       
+
+        public Employee(int employeeId, string name, double salary, Address address)
+        {
+            EmployeeId = employeeId;
+            Name = name;
+            Salary = salary;
+            EmployeeAddress = address;
+        }
+
+        public void DisplayEmployee()
+        {
+            Console.WriteLine($"Employee ID: {EmployeeId}");
+            Console.WriteLine($"Employee Name: {Name}");
+            Console.WriteLine($"Salary: INR.{Salary}");
+            EmployeeAddress.DisplayAddress(); // Display Address details
+        }
+    }
+
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an Address object
+            Address addressObj = new Address("15th Line, Ameerept", "Hyderabad", "500018");
+
+            // Creating an Employee object and passing Address
+            Employee emp = new Employee(10144, "Narasimha", 75000, addressObj);
+
+            // Display Employee Details
+            emp.DisplayEmployee();
+            Console.WriteLine("----------------------------");
+
+            emp.EmployeeAddress.PinCode = "500019";
+
+            // Display Employee Details
+            emp.DisplayEmployee();
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 02.Week-2/07.Day7_OOPS_Inheritance_Abstract/Session_Examples/Eg4_Program_Abstract_Class.cs
@@ -0,0 +1,82 @@
+﻿using System.Net;
+
+namespace ConsoleApp39
+{    
+    abstract class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+
+
+        // Abstract Method to CalculateSalary
+        public abstract double CalculateSalary();
+
+        // Concrete method to display employee details
+        public void DisplayEmployee()
+        {
+            Console.WriteLine($"Employee ID: {EmployeeId}, Name: {Name}, Salary: INR.{CalculateSalary()}");
+        }
+
+    }
+
+
+    class FullTimeEmployee : Employee
+    {
+        public double MonthlySalary { get; set; }
+
+        public FullTimeEmployee(int id, string name, double salary)
+        {
+            EmployeeId = id;
+            Name = name;
+            MonthlySalary = salary;
+        }
+
+        // Implementing abstract method
+        public override double CalculateSalary()
+        {
+            // Additional Logic to process Salary
+            return MonthlySalary; // Fixed monthly salary
+        }
+    }
+
+
+    class PartTimeEmployee : Employee
+    {
+        public double HourlyRate { get; set; }
+        public int HoursWorked { get; set; }
+
+        public PartTimeEmployee(int employeeId, string name, double hourlyRate, int hoursWorked)
+        {
+            HourlyRate = hourlyRate;
+            HoursWorked = hoursWorked;
+            EmployeeId = employeeId;
+            Name = name;
+        }
+
+        public override double CalculateSalary()
+        {
+            return HourlyRate * HoursWorked;
+        }
+    }
+
+     internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Creating Full-time employee
+            FullTimeEmployee emp1 = new FullTimeEmployee(101, "Scott", 50000);
+
+
+            // Creating Part-time employee
+            PartTimeEmployee emp2 = new PartTimeEmployee(102, "Smith", 2000, 80); // 2000 per hour, 80 hours worked
+
+
+            emp1.DisplayEmployee();
+            emp2.DisplayEmployee();
+             
+
+            Console.ReadLine();
+        }
+    }
+}
