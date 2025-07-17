2c6c86bb94d1af456b96c8db23ee4bb3006c3cb8
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-07-17 13:13:41 +0530

    Add files via upload

---
+++ 02.Week-2/07.Day7_OOPS_Inheritance_Abstract/Session_Examples/Eg5_Program._ToString_Override.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+
+namespace ConsoleApp39
+{
+
+    class Employee
+    {
+        // Properties
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"{EmpId}, {Name}, {Salary}"; ;
+        }
+    }
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Program obj = new Program();
+            Console.WriteLine(obj);
+            Console.WriteLine(obj.ToString());
+
+            DateTime d1 = new DateTime();
+            Console.WriteLine(d1);
+
+            Employee empObj = new Employee();
+            empObj.EmpId = 10256;
+            empObj.Name = "Scott";
+            empObj.Salary = 45000;
+
+            Console.WriteLine(empObj); //  invokes ToString() internally
+            Console.WriteLine(empObj.ToString());
+
+
+            Console.ReadLine();
+        }
+    }
+}
