c7fc75b6ee1262f34a449096397da7ce417bce47
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-07-21 10:55:33 +0530

    Add files via upload

---
+++ 03.Week-3/09.Day9_Collections_in_C#/Session_Examples/02.Program_List_Employee.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace ConsoleApp39
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public decimal Salary { get; set; }
+        public int Deptno { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Job: {Job}, Salary: {Salary}, DeptNo: {Deptno}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Employee> empList = new List<Employee>();
+
+            Employee e1 = new Employee() { Id = 1, Name = "Alice", Job = "Manager", Salary = 75000, Deptno = 10 };
+            Employee e2 = new Employee() { Id = 2, Name = "Bob", Job = "Developer", Salary = 65000, Deptno = 20 };
+            Employee e3 = new Employee() { Id = 3, Name = "Charlie", Job = "Analyst", Salary = 60000, Deptno = 20 };
+
+            empList.Add( e1  );
+            empList.Add( e2  );
+            empList.Add( e3  );
+
+            // Iteration
+            foreach (Employee item in empList)
+            {
+                Console.WriteLine(item);
+            }
+          
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 03.Week-3/09.Day9_Collections_in_C#/Session_Examples/03.Program_List_Employee_Add_Remove.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace ConsoleApp39
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public decimal Salary { get; set; }
+        public int Deptno { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Job: {Job}, Salary: {Salary}, DeptNo: {Deptno}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Collection Initializer
+            List<Employee> empList = new List<Employee>()
+            {
+                  new Employee() { Id = 101, Name = "Alice", Job = "Manager", Salary = 75000, Deptno = 10 },
+                  new Employee() { Id = 102, Name = "Bob", Job = "Developer", Salary = 65000, Deptno = 20 },
+                  new Employee() { Id = 103, Name = "Charlie", Job = "Analyst", Salary = 60000, Deptno = 20 }
+            };
+
+           
+
+            // Iteration
+            foreach (Employee item in empList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("----------------------------------------------");
+
+            int eno = 105;
+            Employee obj = empList.FirstOrDefault( item => item.Id == eno );
+
+            if(obj != null)
+                empList.Remove(obj);
+            else
+                Console.WriteLine($"Employee Id : {eno} does not exists");
+
+            // Iteration
+            foreach (Employee item in empList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
