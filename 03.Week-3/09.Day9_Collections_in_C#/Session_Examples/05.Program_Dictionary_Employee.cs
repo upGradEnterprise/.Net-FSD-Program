76d8cbeb5e26a2e878ecc528a53f2ac42086aa4a
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-07-21 13:10:01 +0530

    Add files via upload

---
+++ 03.Week-3/09.Day9_Collections_in_C#/Session_Examples/05.Program_Dictionary_Employee.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace ConsoleApp39
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public decimal Salary { get; set; }
+        public int Deptno { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Job: {Job}, Salary: {Salary}, DeptNo: {Deptno}";
+        }
+    }
+
+    class Program
+    { 
+            static void Main(string[] args)
+            { 
+
+                Dictionary<int, Employee> empsDictionary = new Dictionary<int, Employee>() {
+
+                    { 101, new Employee() { Id = 101, Name = "Alice", Job = "Manager", Salary = 75000, Deptno = 10 } }, 
+                    { 102, new Employee() { Id = 102, Name = "Bob", Job = "Developer", Salary = 65000, Deptno = 20 } }, 
+                    { 103, new Employee() { Id = 103, Name = "Charlie", Job = "Analyst", Salary = 60000, Deptno = 20 } }
+                };
+
+
+             // Iterate all items based on Key
+            foreach(var key in empsDictionary.Keys)
+            {
+                Console.WriteLine(empsDictionary[key]);
+            }
+            Console.WriteLine("----------------------");
+
+            // Add a new employee
+            empsDictionary.Add(104, new Employee { Id = 104, Name = "David", Job="Lead", Salary = 70000, Deptno=20 });
+
+
+
+            foreach (var item in empsDictionary)
+            {
+                Console.WriteLine($"Key : {item.Key}, Value : {item.Value}");
+            }
+
+            Console.WriteLine("--------------------------------");
+
+
+            Console.WriteLine("Enter Emp Id to remove : ");
+            int eno = int.Parse(Console.ReadLine());
+
+            if(empsDictionary.ContainsKey(eno))
+            {
+                // Remove an employee 
+                empsDictionary.Remove(eno);
+               
+                foreach (var key in empsDictionary.Keys)
+                {
+                    Console.WriteLine(empsDictionary[key]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Emp Id does not exists.");
+            }
+            
+
+
+            Console.ReadLine();
+            }
+        }    
+}
---
+++ 03.Week-3/09.Day9_Collections_in_C#/Session_Examples/06.Program_Dictionary_List.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace ConsoleApp39
+{    
+    class Program
+    { 
+            static void Main(string[] args)
+            {
+            Dictionary<string, List<int>> studentGrades = new Dictionary<string, List<int>>()
+            {
+                { "Scott", new List<int>{ 56, 89, 74, 58 } },
+                { "Smith", new List<int>{ 56, 89, 74, 58, 96 } },
+                { "Sandy", new List<int>{ 56, 89, 74, 58 } }
+            };
+
+
+            studentGrades["Scott"].Add(68);
+
+
+            foreach (var key in studentGrades.Keys)
+            {
+                List<int> lst = studentGrades[key];
+                string csvStr = string.Join(",", lst);
+                Console.WriteLine(csvStr);
+
+                // Console.WriteLine(string.Join(",", studentGrades[key]));
+            }
+
+
+                    
+
+
+ 
+
+            Console.ReadLine();
+            }
+        }    
+}
