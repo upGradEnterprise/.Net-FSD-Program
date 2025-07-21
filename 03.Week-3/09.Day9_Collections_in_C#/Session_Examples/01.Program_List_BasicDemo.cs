305cdc1ec7f3804c809df422a7d4c98b504de81b
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-07-21 10:22:47 +0530

    Add files via upload

---
+++ 03.Week-3/09.Day9_Collections_in_C#/Session_Examples/01.Program_List_BasicDemo.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+
+namespace ConsoleApp39
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cityList = new List<string>();
+
+            cityList.Add("Hyderabad");
+            cityList.Add("Mumbai");
+            cityList.Add("New Delhi");
+            cityList.Add("Kolkata");
+
+
+            Console.WriteLine($"No. of Cities : {cityList.Count}");
+
+            Console.Write("City Names  : ");
+            foreach (string item in cityList)
+            {
+                Console.Write($"{item} , ");
+            }
+
+            Console.WriteLine();
+            cityList.Add("Chennai");
+            cityList.Add("Goa");
+
+            Console.WriteLine($"No. of Cities : {cityList.Count}");
+
+            Console.Write("City Names  : ");
+            foreach (string item in cityList)
+            {
+                Console.Write($"{item} , ");
+            }
+
+            Console.WriteLine();
+            cityList.Remove("Chennai");
+            Console.WriteLine($"No. of Cities : {cityList.Count}");
+
+            Console.Write("City Names  : ");
+            foreach (string item in cityList)
+            {
+                Console.Write($"{item} , "); 
+            }
+
+            Console.WriteLine();
+            bool flag = cityList.Contains("Chennai");
+            Console.WriteLine($"Is Chennai exists in List : {flag}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
