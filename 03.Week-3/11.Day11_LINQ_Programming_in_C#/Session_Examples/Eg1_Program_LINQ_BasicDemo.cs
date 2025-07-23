5e8ec30dedb4e236e5c94476f7dea59a17155333
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-07-23 10:59:35 +0530

    Add files via upload

---
+++ 03.Week-3/11.Day11_LINQ_Programming_in_C#/Session_Examples/Eg1_Program_LINQ_BasicDemo.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp39
+{    
+    public class Program
+    {
+        static void Main()
+        {
+            int[] arr1 = [8, 5, 4, 9, 6, 2, 3, 7, 12, 18, 22];
+
+
+            // LINQ Query
+            var arr2 = from n in arr1
+                       where n % 2 == 0 && n < 10
+                       orderby n
+                       select n;
+
+
+            //  Expression Methods
+            var arr3 = arr1
+                        .Where(x => x % 2 == 0 && x < 10)
+                        .OrderBy(x => x);
+
+            Console.WriteLine(string.Join(",", arr1));
+            Console.WriteLine(string.Join(",", arr2));
+            Console.WriteLine(string.Join(",", arr3));
+
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ 03.Week-3/11.Day11_LINQ_Programming_in_C#/Session_Examples/Eg2_Program_Employee_LINQ_Query.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp39
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+        public int PerformanceScore { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Name}, {Department}, {Salary}, {PerformanceScore}";
+        }
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee { Id = 1, Name = "Sarah", Department = "IT", Salary = 75000, PerformanceScore = 85 },
+                new Employee { Id = 2, Name = "Mike", Department = "HR", Salary = 65000, PerformanceScore = 90 },
+                new Employee { Id = 3, Name = "Lisa", Department = "IT", Salary = 80000, PerformanceScore = 88 },
+                new Employee { Id = 4, Name = "Tom", Department = "Sales", Salary = 70000, PerformanceScore = 82 },
+                new Employee { Id = 5, Name = "Bob", Department = "Sales", Salary = 60000, PerformanceScore = 72 },
+                new Employee { Id = 6, Name = "Ralph", Department = "HR", Salary = 50000, PerformanceScore = 62 }
+            };
+
+
+            var q1 = from emp in employees                                         
+                     select emp;
+
+
+            var q2 = from emp in employees
+                     orderby emp.Salary descending
+                     select emp;
+
+            var q3 = from emp in employees
+                     where emp.Department == "Sales"
+                     select emp.Name;
+
+
+            foreach (var item in q3)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+
+}
