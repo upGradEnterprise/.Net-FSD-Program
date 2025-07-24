e5197a3a193580efe5ab255d97fefa33f8ad9448
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-07-24 14:08:39 +0530

    Add files via upload

---
+++ 03.Week-3/12.Day12_Delegates_Events/Session_Examlpes/02.Events/Eg1_Program_Events_Buton_Click.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp39
+{
+
+    public delegate void MyEventHandler(object sender, EventArgs e);
+
+    public class Button
+    {
+        public event MyEventHandler Click;
+
+        public void PressButton()
+        {
+            Console.WriteLine("Button Pressed...!");
+            Click.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+     
+    class Program
+    {
+
+        public static void m1(object sender, EventArgs e)
+        {
+            Console.WriteLine("Logic to validate user id and password");
+
+        }
+
+        public static void m2(object sender, EventArgs e)
+        {
+            Console.WriteLine("send the notfication to user");
+
+        }
+
+        static void Main(string[] args)
+        {
+            Button b1 = new Button ();
+            b1.Click += m1;
+            b1.Click += m2;
+            b1.PressButton ();
+
+
+            Console.ReadLine();
+        }
+    }
+}
