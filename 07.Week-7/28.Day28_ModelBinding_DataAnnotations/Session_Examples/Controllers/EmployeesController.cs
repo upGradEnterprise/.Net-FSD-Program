8b60662a12746fc5d427ca76ef18faf7420f3f94
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-08-18 14:00:42 +0530

    Add files via upload

---
+++ 07.Week-7/28.Day28_ModelBinding_DataAnnotations/Session_Examples/Controllers/EmployeesController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication50.Models;
+
+namespace WebApplication50.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public static List<Employee> employees = new List<Employee>
+                {
+                    new Employee { Id = 1, Name = "Alice", Job = "Manager", Salary = 75000, Deptno = 10 },
+                    new Employee { Id = 2, Name = "Bob", Job = "Developer", Salary = 65000, Deptno = 20 },
+                    new Employee { Id = 3, Name = "Charlie", Job = "Analyst", Salary = 60000, Deptno = 20 },
+                    new Employee { Id = 4, Name = "Diana", Job = "HR", Salary = 55000, Deptno = 30 },
+                    new Employee { Id = 5, Name = "Ethan", Job = "Support", Salary = 45000, Deptno = 40 }
+                    };
+
+        public IActionResult Index()
+        {
+            return View(employees);
+        }
+
+        //     /Employee/Detailed/10
+        public IActionResult Details(int id)
+        {
+            var emp = employees.FirstOrDefault(x => x.Id == id);
+            return View(emp);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee emp)
+        {
+            if(ModelState.IsValid)
+            {
+                 employees.Add(emp);
+                return RedirectToAction("Index");
+
+                // ViewBag.Message = "Employee Details are valid";
+                // return View(emp);
+
+            }
+            else
+            {
+                ViewBag.Message = "Invalid Employee Details";
+                return View(emp);   
+            }            
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var emp = employees.FirstOrDefault(x => x.Id == id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee emp)
+        {
+            var existEmp = employees.FirstOrDefault( x => x.Id == emp.Id);
+
+            existEmp.Name = emp.Name;
+            existEmp.Salary= emp.Salary;
+            existEmp.Job = emp.Job;
+            existEmp.Deptno = emp.Deptno;
+
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var emp = employees.FirstOrDefault(x => x.Id == id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var emp = employees.FirstOrDefault(x => x.Id == id);
+            employees.Remove(emp);
+            return RedirectToAction("Index");
+        }
+
+        //  parameter name should be same as query string key name
+        public IActionResult Search(string job, int deptno)
+        {
+
+            var searchResultList = employees.Select(item => item);
+
+            if(job != null)
+            {
+                searchResultList = searchResultList.Where(x => x.Job == job);
+            }
+
+            if(deptno != 0)
+            {
+                searchResultList = searchResultList.Where(x => x.Deptno == deptno);
+             
+            }
+
+            return View(searchResultList);
+        }
+
+
+
+    }
+}
---
+++ 07.Week-7/28.Day28_ModelBinding_DataAnnotations/Session_Examples/Models/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication50.Models
+{ 
+
+    public class Employee  : IValidatableObject
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Employee Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be 3–50 characters long")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Job Title is required")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Job must contain only letters")]
+        public string Job { get; set; }
+
+        [Range(3000, 200000, ErrorMessage = "Salary must be between 3000 and 200000")]
+        public decimal Salary { get; set; }
+
+        [Range(10, 99, ErrorMessage = "Deptno should be between 10 and 99")]
+        public int Deptno { get; set; }
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Deptno == 10 && Salary < 50000)
+            {
+                yield return new ValidationResult(
+                    "Employees in Deptno 10 must have a salary above 50,000",
+                    new[] { nameof(Salary), nameof(Deptno) });
+            }
+        }
+
+
+    }
+
+
+}
+
+
+
+/*
+ * 
+ *   public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Deptno == 10 && Salary < 50000)
+            {
+                yield return new ValidationResult(
+                    "Employees in Deptno 10 must have a salary above 50,000",
+                    new[] { nameof(Salary), nameof(Deptno) });
+            }
+        }
+
+*/
