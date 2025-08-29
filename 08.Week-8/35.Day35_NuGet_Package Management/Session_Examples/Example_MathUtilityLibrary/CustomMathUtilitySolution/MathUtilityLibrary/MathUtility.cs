c841da90176728996f2f0b2f8f317eda63227b4f
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-08-29 10:58:25 +0530

    Add files via upload

---
+++ 08.Week-8/35.Day35_NuGet_Package Management/Session_Examples/Example_MathUtilityLibrary/CustomMathUtilitySolution/MathUtilityLibrary/MathUtility.cs
@@ -0,0 +1,15 @@
+﻿namespace MathUtilityLibrary
+{
+    public class MathUtility
+    {
+        public int Sum(int x, int y)
+        {
+          return  x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
