06db263f29eb5e9fcd63c071180cd69ad69e3543
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-09-16 17:28:55 +0530

    Update ProductService.js

--- 10.Week-10/Day44_API Integration Axios & Error Handling/Complete_Application_Routing_API_Calls_with_Security/src/services/ProductService.js
+++ 10.Week-10/Day44_API Integration Axios & Error Handling/Complete_Application_Routing_API_Calls_with_Security/src/services/ProductService.js
@@ -1,16 +1,49 @@
 import axios from "axios";
 
-
-
 const API_URL = "https://localhost:7026/api/Products"; 
+// const API_URL = "http://localhost:3000/products";    // JSON Server URL
 const AUTH_URL = "https://localhost:7026/api/Authenticate"; 
 
 
+const axiosObj = axios.create();
+
+// Function to get the auth header with the token from session storage -- to configure axios requests
+// Attach token before each request
+axiosObj.interceptors.request.use(
+  (config) => {
+    const token = sessionStorage.getItem("AUTH_TOKEN");
+    console.log("Attaching token to request: " + token);
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Function to configure axios responses
+// Handle expired/invalid token responses globally
+axiosObj.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.warn("JWT expired or invalid. Logging out...");
+      sessionStorage.removeItem("AUTH_TOKEN");
+
+      // Optionally trigger a full reload or redirect
+      window.location.href = "/Login"; 
+    }
+    return Promise.reject(error);
+  }
+);
+
+/*
 function getAuthHeader() {
           const token = sessionStorage.getItem("AUTH_TOKEN");
    //    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IkFkbWluIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6IkFkbWluIiwianRpIjoiZjY1NGI1MTQtM2EwMC00MGU1LWJmMTYtODdmZDNiYTM1ZDM3IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiQWRtaW4iLCJleHAiOjE3NTgwMDgwNjYsImlzcyI6IllvdXJJc3N1ZXIiLCJhdWQiOiJZb3VyQXVkaWVuY2UifQ.wfuGrw3DCyqa6Z_gGOF_0_G7yWv1-T8xfc4XNMjsZBM";
        return { Authorization: `Bearer ${token}` };
 }
+*/
  
 
 class ProductService {
@@ -33,23 +66,23 @@ class ProductService {
 
   async getAll() {
       console.log("Message from ProductService");
-        return axios.get(API_URL, { headers: getAuthHeader() });   
+        return axiosObj.get(API_URL);   
   }
 
   async getById(id) {
-    return axios.get(`${API_URL}/${id}`, { headers: getAuthHeader() });
+    return axiosObj.get(`${API_URL}/${id}`);
   }
 
   async create(product) {
-    return axios.post(API_URL, product, { headers: getAuthHeader() });
+    return axiosObj.post(API_URL, product);
   }
 
    async update(id, product) {
-    return axios.put(`${API_URL}/${id}`, product, { headers: getAuthHeader() });
+    return axiosObj.put(`${API_URL}/${id}`, product);
   }
 
   async delete(id) {
-    return axios.delete(`${API_URL}/${id}`, { headers: getAuthHeader() });
+    return axios.delete(`${API_URL}/${id}`);
   }
 
 }
